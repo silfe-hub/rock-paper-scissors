865c0fbdda8005ff7940526dc68f35c2482ab9a1
silfe-hub <siljelf@gmail.com>
2024-06-30 15:16:41 +0200

    Tidy up and conversion.
    Cleaned up code and create function for convert choice from human and computer.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,40 @@
-
-function getComputerChoice() {
+function getComputerInput() {
     //Generate a random number between 1 and 3
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-
-    //Convert random number to 'rock', 'paper' or 'scissors'
+    let computerInput = Math.floor(Math.random() * 3) + 1;  
+    // convert random number to string
+    if (computerInput === 1) {
+        return "paper";
+    } else if (computerInput === 2) {
+        return "scissors";
+    } else if (computerInput === 3) {
+        return "rock";
+    }
 }
 
-let computerChoiceConverter; 
+let computerChoice = getComputerInput()
 
-if (getComputerChoice === 1) {
-    computerChoiceConverter ="paper";
-} else if (getComputerChoice === 2) {
-    computerChoiceConverter = "scissors";
-} else {
-    computerChoiceConverter = "rock";
+console.log("Computer chose: " + computerChoice);
+
+//Convert computerChoice back to integer
+function convertComputerChoice(computerChoice) {
+    let computerInputConverter;
+
+    if (computerChoice === "paper") {
+        computerInputConverter = 1;
+    } else if (computerChoice === "scissors") {
+        computerInputConverter = 2;
+    } else {
+        computerInputConverter = 3;
+    }
+
+    return computerInputConverter;
 }
 
-console.log("Computer chose: " + computerChoiceConverter);
+console.log(convertComputerChoice(computerChoice));
 
+
+
+//Function for human choice
 function getHumanChoice() {
     let humanInput = prompt("Choose rock, paper or scissors:", ""); 
     if (humanInput === "paper") {
@@ -35,8 +52,9 @@ let humanChoice = getHumanChoice();
 
 console.log("Human chose: " + humanChoice);
 
+//Convert humanChoice to integer
 function convertHumanInput(humanChoice) {
-    let humanInputConverter
+    let humanInputConverter;
     
     if (humanChoice === "paper") {
         humanInputConverter = 1;
@@ -55,45 +73,3 @@ function convertHumanInput(humanChoice) {
 console.log(convertHumanInput(humanChoice));
 
 
-/*
-let computerChoiceConverter = 
-    if (randomNumber === 1) {
-        return "paper";
-    } else if (randomNumber === 2) {
-        return "scissors";
-    } else {
-        return "rock";
-    }
-
-
-function getHumanChoice() {
-    let getHumanChoice = prompt("Choose rock, paper or scissors:", "");
-    if (getHumanChoice === "paper") {
-        return 1; 
-    } else if (getHumanChoice === "scissors") {
-        return 2;
-    } else {
-        return 3;
-    }
-}
-
-
-function playRound() {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-
-    console.log("Human choice:", getHumanChoice())
-    console.log("Computer choice:", computerChoice)
-
-    if (humanChoice === computerChoice) {
-        return "It's a tie!";
-    } else if (humanChoice === (computerChoice && 3) + 1) {
-        return "Human wins!";
-    } else {
-        return "Computer wins!";
-    }    
-}
-
-
-
-console.log(playRound()); */
